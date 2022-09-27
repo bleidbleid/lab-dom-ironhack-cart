e660e3506c173f1afc9d6488382210d3662fb63d
Maria Bladé <mariablade2000+git@gmail.com>
2022-09-27 17:32:13 +0200

    iteration3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,34 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  console.log(price);
+  const quantity = product.querySelector('.quantity input').value;
+  console.log(quantity);
+  // let nomVariable = myElement.innerHTML per a accedir al contingut d’un element.
+  // calculate the subtotal price. Create a new variable.
+  // Its value will be equal to the product of these values.
+  const subtotalPrice = parseInt(price) * parseInt(quantity);
+  // get the DOM element that should hold the subtotal.
+  // Hint: it is the element with the class subtotal.
+  // tag.innerHTML = <contingut>; per a afegir contingut a un element.
+  console.log(product.querySelector('.subtotal span').innerHTML = subtotalPrice)
+  return subtotalPrice;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  //Complete the function named calculateAll(). Its purpose is to call the function updateSubtotal with every tr.product DOM node in the table#cart.
+  // Also, let's add a new product to our index.html file. Duplicate the tr with the class product, rename the product inside, and change the product price.
+  const products = document.querySelectorAll('.product');
+  let totalPrice = 0;
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  for (let i = 0; i < products.length; i++) {
+    let element = products[i];
+    // ITERATION 3
+    totalPrice += updateSubtotal(element);
+  }
+  document.querySelector('#total-value span').innerHTML = totalPrice;
+  
 }
 
 // ITERATION 4
